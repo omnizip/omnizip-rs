ba0093463bedad044e46cb2da801ef945fc9fe7e
Ronald Tse <ronald.tse@ribose.com>
2026-08-25 08:50:51 +0800

    chore(containers): expose raw md5 from omnizip-crypto; tidy msi example

--- omnizip-ole/examples/readmsi.rs
+++ omnizip-ole/examples/readmsi.rs
@@ -3,7 +3,10 @@ fn main() {
     let p = std::env::args().nth(1).unwrap();
     match omnizip_ole::msi::MsiReader::open(std::path::Path::new(&p)) {
         Ok(r) => {
-            println!("tables: {:?}", r.tables.iter().map(|t| t.name.clone()).collect::<Vec<_>>());
+            println!(
+                "tables: {:?}",
+                r.tables.iter().map(|t| t.name.clone()).collect::<Vec<_>>()
+            );
             for (key, comp, size, name) in r.file_rows().iter().take(5) {
                 println!("file: {key} comp={comp} size={size} name={name}");
             }
